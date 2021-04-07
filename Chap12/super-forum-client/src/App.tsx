60e755335aaff33615c4394ae7eeac067687d5a1
sultanby <jemjasott@gmail.com>
2021-04-07 15:12:43 +0200

    Chap12 init

---
+++ Chap12/super-forum-client/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import LeftMenu from "./components/LeftMenu";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import RightMenu from "./components/RightMenu";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <SideBar></SideBar>
+      <LeftMenu></LeftMenu>
+      <Main></Main>
+      <RightMenu></RightMenu>
+    </div>
+  );
+}
+
+export default App;
---
+++ Chap12/super-forum-client/src/components/RightMenu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
+
+const RightMenu  = () => {
+    const { width } = useWindowDimensions();
+    if (width <= 768) {
+        return null;
+    }
+    return <div className="rightmenu">Right Menu</div>;
+};
+export default RightMenu ;
